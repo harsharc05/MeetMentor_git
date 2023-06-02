9264e520e02d4ce48c603cbfe88622aef454de5b
harsharc05 <133763592+harsharc05@users.noreply.github.com>
2023-06-02 19:43:10 +0530

    primary swatch colour changed to red

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'My Flutter App',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.red,
       ),
       home: HomePage(), // Set the home page as the HomePage widget
     );
