28be9105167a8abbb11b4ec7fd4259d7f67717c4
harsharc05 <133763592+harsharc05@users.noreply.github.com>
2023-06-05 22:34:44 +0530

    i added the image to the profile page

--- lib/pages/student_profile.dart
+++ lib/pages/student_profile.dart
@@ -1,41 +1,46 @@
-//import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
+ import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:meet_mentor_app/pages/stuhome_page.dart';
-import 'login_page.dart';  
- var i;
- var name;
+import 'login_page.dart';
+
 class StudentProfilePage extends StatefulWidget {
   @override
   State<StudentProfilePage> createState() => _StudentProfilePageState();
 }
 
 class _StudentProfilePageState extends State<StudentProfilePage> {
-   
+  var i;
+  var fname, sname;
+  String? fullName;
 
   @override
   Widget build(BuildContext context) {
     FirebaseAuth.instance.authStateChanges().listen((User? user) {
-    if (user == null) {
-      print('User is currently signed out!');
-    } else {
-      print(user.uid);
-      i=user.uid;
-    }
-  });
-   
-   FirebaseFirestore.instance.collection('users').get().then((snapshot) => snapshot.docs
-   .forEach((element) { 
-    print(element.data()['uid']);
-    if(element.data()['uid']==i){
-      print(element.data()['firstName']);
-      name=element.data()['firstName'];
-       setState(() {
-        name = element.data()['firstName'];  
-      });
-    }
-   }));
+      if (user == null) {
+        print('User is currently signed out!');
+      } else {
+        print(user.uid);
+        i = user.uid;
+      }
+    });
+
+    FirebaseFirestore.instance
+        .collection('users')
+        .get()
+        .then((snapshot) => snapshot.docs.forEach((element) {
+              print(element.data()['uid']);
+              if (element.data()['uid'] == i) {
+                print(element.data()['firstName']);
+                fname = element.data()['firstName'];
+                setState(() {
+                  fname = element.data()['firstName'];
+                  sname = element.data()['lastName'];
+                  fullName = fname + ' ' + sname;
+                });
+              }
+            }));
+
     return Scaffold(
       backgroundColor: Color(0xFFC9C0FF),
       body: Padding(
@@ -57,24 +62,39 @@ class _StudentProfilePageState extends State<StudentProfilePage> {
               ),
             ),
             Center(
-              child: Container(
-                width: 150,
-                height: 150,
-                decoration: BoxDecoration(
-                  shape: BoxShape.circle,
-                  image: DecorationImage(
-                    image: AssetImage('assets/images/profile_pic.png'),
-                    fit: BoxFit.cover,
-                  ),
-                ),
+              child: FutureBuilder<DocumentSnapshot>(
+                future: FirebaseFirestore.instance
+                    .collection('users')
+                    .doc(i)
+                    .get(),
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    var userData = snapshot.data!.data() as Map<String, dynamic>;
+                    var imageUrl = userData['image'];
+
+                    return Container(
+                      width: 150,
+                      height: 150,
+                      decoration: BoxDecoration(
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          image: NetworkImage(imageUrl),
+                          fit: BoxFit.fill
+                      ),
+                    ));
+                  }
+
+                  // Show a placeholder or loading indicator while loading the image
+                  return CircularProgressIndicator();
+                },
               ),
             ),
             SizedBox(height: 16),
             Center(
               child: Text(
-                name,
+                fullName!,
                 style: TextStyle(
-                  fontSize: 20,
+                  fontSize: 25,
                   fontWeight: FontWeight.bold,
                 ),
               ),
@@ -88,8 +108,9 @@ class _StudentProfilePageState extends State<StudentProfilePage> {
                   child: ElevatedButton(
                     onPressed: () {
                       Navigator.push(
-                      context,
-                      MaterialPageRoute(builder: (context) => StudentHomePage()));
+                        context,
+                        MaterialPageRoute(builder: (context) => StudentHomePage()),
+                      );
                     },
                     style: ElevatedButton.styleFrom(
                       backgroundColor: Color(0xFF150578),
--- lib/pages/stuprsnl_chat.dart
+++ lib/pages/stuprsnl_chat.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
-import 'package:firebase_auth/firebase_auth.dart';
+//import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 //import 'package:audioplayers/audioplayers.dart';
